004ad5683f8d06abbab2bff8f9b318e8a79a5f07
Patrick Webster <noxwizard@phpbb.com>
2010-06-20 01:18:52 +0000

    I suppose some people would like to run that on a non-Windows machine. Switching slash direction and line endings.

--- documentation/create_pdf.sh
+++ documentation/create_pdf.sh
@@ -4,6 +4,6 @@ echo "Removing previous PDF"
 rm olympus_doc.pdf
 
 echo "Creating new PDF"
-fop -xml olympus_doc.xml -xsl xsl\olympus_pdf.xsl -pdf olympus_doc.pdf
+fop -xml olympus_doc.xml -xsl xsl/olympus_pdf.xsl -pdf olympus_doc.pdf
 
 echo "Done"
