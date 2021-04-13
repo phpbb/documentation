098a5244c0f0921536b1b0d4caee00e14ed7134c
Marc Alexander <admin@m-a-styles.de>
2021-04-13 20:06:35 +0200

    Remove not needed sidebars directive
    The rtd theme does not support this anyway.

--- development/conf.py
+++ development/conf.py
@@ -169,11 +169,7 @@ html_static_path = ['_static']
 #html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-html_sidebars = {
-    '**': [
-        'versioning.html',
-    ],
-}
+#html_sidebars = {}
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
